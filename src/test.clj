e9c6436ae0905c9dd70c4fd8c8d7eb4307b6c453
ajglenn <ajglenn@gmail.com>
2014-05-17 13:10:32 -0500

    Testing

---
+++ src/test.clj
@@ -0,0 +1,12 @@
+(require '[clojure.java.io :as io])
+
+(with-open [rdr (io/reader "test.txt")]
+  (doseq [line (line-seq rdr)]
+    (println line)))
+
+(defn write-thousand-lines [filename]
+  (with-open [wrt (io/writer filename)]
+    (doseq [x (range 1 1000)]
+      (.write wrt (str x "\n")))))
+
+(write-thousand-lines "written.txt")
